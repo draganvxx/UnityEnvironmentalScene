76978eadba329a045edeb9af3e0950accd7cd8ab
danny-pulse <dannyvisekruna@hotmail.com>
2021-10-05 23:20:39 -0400

    pushing project

---
+++ EnvironmentalScene/Assets/Scripts/DropRocks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropRocks : MonoBehaviour
+{
+    public GameObject[] rocks;
+    public MeshCollider[] rockCollider;
+
+    void Start()
+    {
+        rocks = GameObject.FindGameObjectsWithTag("Rock");
+        
+    }
+    public void OnClicked()
+    {
+        foreach (GameObject obj in rocks)
+        {
+            obj.GetComponent<MeshCollider>().enabled = true;
+            obj.AddComponent<Rigidbody>();
+        }
+
+
+    }
+}
